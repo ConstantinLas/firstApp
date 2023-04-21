6ce4f38fdaea0b2b8ffdfcd26a16a8191ff14570
Constantin <constantinlasker@Constantins-MacBook-Air.local>
2023-04-21 19:20:04 +0200

    Leson 24

--- script.js
+++ script.js
@@ -21,6 +21,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+
+/* Задание 2 на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+
 'use strict';
 
 let numberOfFilms = +prompt("How many films did you see?","");
@@ -33,17 +49,42 @@ let personalMovieDB = {
     privat: false
     };
 
-let aswFilm1 =  prompt("Один из последних просмотренных фильмов?", ""),
-    answNone1 = prompt("На сколько оцените его?", ""),
-    aswFilm2 =  prompt("Один из последних просмотренных фильмов?", ""),
-    answNone2 = prompt("На сколько оцените его?", "");
+ // let aswFilm1 =  prompt("Один из последних просмотренных фильмов?", ""),
+ //   answNone1 = prompt("На сколько оцените его?", ""),
+ //   aswFilm2 =  prompt("Один из последних просмотренных фильмов?", ""),
+ //   answNone2 = prompt("На сколько оцените его?", "");
 
 //personalMovieDB.movies = {
 //    aswFilm1 : answNone1,
 //    aswFilm2 : answNone2
 //    };
 
-personalMovieDB.movies[aswFilm1] = answNone1;
-personalMovieDB.movies[aswFilm2] = answNone2;
+for (let i = 0; i < 2; i++) {
+    const   a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его оцениваете?", "");
+
+    
+    if (a != null && a != "" && b != null && b != "" && a.length > 5){
+        personalMovieDB.movies[a] =  b;
+    } else {
+        console.log("Error");
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+
+// personalMovieDB.movies[aswFilm1] = answNone1;
+// personalMovieDB.movies[aswFilm2] = answNone2;
 
 console.log(personalMovieDB);
